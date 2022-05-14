900594eeaf880df5e89afa60df1fd307a6efe8a1
unknown <jiteshshivgan5@gmail.com>
2022-05-14 10:05:11 +0530

    First commit: Setting up the server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+//const because we do not want this variable to be over-ridden by anyone in the code
+const app=express();
+const port =8000;
+//When we deploy to live server, it would be port number 80.
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Error in running the server: ', err);
+        //interpolation
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is running on port : ${port}`);
+});
+
+//I need to make it GIT repository so that we can track all the changes.
